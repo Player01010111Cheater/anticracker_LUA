d182b9d2ec4be6339eed966a2ac7b753318f9be1
Easy lua <staskuzmin1406@gmail.com>
2025-07-26 14:44:27 +0300

    Update anticrack.lua

--- anticrack.lua
+++ anticrack.lua
@@ -4,35 +4,12 @@ local suspicious = {
 local function CheckForCrackModules()
     local Players = game:GetService("Players")
     local player = Players.LocalPlayer or nil
-
-    local function kick(reason)
-        if player and player.Kick then
-            player:Kick("Reason: " .. reason)
-        else
-            error("Client was been deleted by developer.")
-        end
-    end
-    for _, name in ipairs(suspicious) do
-        if getfenv()[name] ~= nil then
-            kick("Too much errors.")
-        end
-    end
-    local info = debug.getinfo(warn)
-    if info and info ~= "=[C]" then
-        game.Players.LocalPlayer:Kick("Detected kick blocker.")
-    end
-    local success, info = pcall(function()
-        return debug.getinfo(game.Players.LocalPlayer.Kick)
-    end)
-
-    if success and info and info.source ~= "=[C]" then
+    
+    local info1 = debug.getinfo(game.Players.LocalPlayer.Kick)
+    if info1 and info1.source ~= "=[C]" then
         warn("⚠️ Kick подменён")
         -- Лог, кик или отправка в Discord
     end
-    if islclosure and islclosure(game.Players.LocalPlayer.Kick) then
-        warn("Kick подменён Lua-функцией")
-    end
-
 end
 
 CheckForCrackModules()
